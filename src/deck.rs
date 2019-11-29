0b86e87a76a9abbdded11637c33b9085fe5eadbe
Matt Traudt <sirmatt@ksu.edu>
2019-11-29 15:34:08 -0600

    Progress on Hand struct

---
+++ src/deck.rs
@@ -0,0 +1,270 @@
+use rand::prelude::*;
+use std::error::Error;
+use std::fmt;
+
+pub const ALL_RANKS: [Rank; 13] = [
+    Rank::R2,
+    Rank::R3,
+    Rank::R4,
+    Rank::R5,
+    Rank::R6,
+    Rank::R7,
+    Rank::R8,
+    Rank::R9,
+    Rank::RT,
+    Rank::RJ,
+    Rank::RQ,
+    Rank::RK,
+    Rank::RA,
+];
+const ALL_SUITS: [Suit; 4] = [Suit::Club, Suit::Diamond, Suit::Heart, Suit::Spade];
+const DECK_LEN: usize = ALL_RANKS.len() * ALL_SUITS.len();
+//const SPADE: &str = "♠";
+const SPADE: &str = "♤";
+//const HEART: &str = "♥";
+const HEART: &str = "♡";
+//const DIAMOND: &str = "♦";
+const DIAMOND: &str = "♢";
+//const CLUB: &str = "♣";
+const CLUB: &str = "♧";
+
+#[derive(Hash, PartialEq, Eq, PartialOrd, Clone, Copy, Debug)]
+pub enum Suit {
+    Club,
+    Diamond,
+    Heart,
+    Spade,
+}
+
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Club => write!(f, "{}", CLUB),
+            Self::Diamond => write!(f, "{}", DIAMOND),
+            Self::Heart => write!(f, "{}", HEART),
+            Self::Spade => write!(f, "{}", SPADE),
+        }
+    }
+}
+
+#[derive(Hash, PartialEq, Eq, PartialOrd, Clone, Copy, Debug)]
+pub enum Rank {
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    R8,
+    R9,
+    RT,
+    RJ,
+    RQ,
+    RK,
+    RA,
+}
+
+impl Rank {
+    pub fn value(&self) -> u8 {
+        match self {
+            Rank::R2 => 2,
+            Rank::R3 => 3,
+            Rank::R4 => 4,
+            Rank::R5 => 5,
+            Rank::R6 => 6,
+            Rank::R7 => 7,
+            Rank::R8 => 8,
+            Rank::R9 => 9,
+            Rank::RT | Rank::RJ | Rank::RQ | Rank::RK => 10,
+            Rank::RA => 11,
+        }
+    }
+}
+
+impl fmt::Display for Rank {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::R2 => write!(f, "2"),
+            Self::R3 => write!(f, "3"),
+            Self::R4 => write!(f, "4"),
+            Self::R5 => write!(f, "5"),
+            Self::R6 => write!(f, "6"),
+            Self::R7 => write!(f, "7"),
+            Self::R8 => write!(f, "8"),
+            Self::R9 => write!(f, "9"),
+            Self::RT => write!(f, "T"),
+            Self::RJ => write!(f, "J"),
+            Self::RQ => write!(f, "Q"),
+            Self::RK => write!(f, "K"),
+            Self::RA => write!(f, "A"),
+        }
+    }
+}
+
+#[derive(Hash, PartialEq, Eq, PartialOrd, Clone, Copy, Debug)]
+pub struct Card {
+    suit: Suit,
+    pub rank: Rank,
+}
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}{}", self.rank, self.suit)
+    }
+}
+
+impl Card {
+    pub fn new(rank: Rank, suit: Suit) -> Self {
+        Self { rank, suit }
+    }
+
+    pub fn value(&self) -> u8 {
+        self.rank.value()
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub enum DeckError {
+    OutOfCards,
+}
+
+impl Error for DeckError {}
+
+impl fmt::Display for DeckError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DeckError::OutOfCards => write!(f, "No more cards in deck"),
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct Deck {
+    cards: Vec<Card>,
+    next: usize,
+}
+
+impl Deck {
+    /// Generate a new single deck of cards, shuffled
+    pub fn new() -> Self {
+        let mut d = Self::with_length(1);
+        d.shuffle();
+        d
+    }
+
+    /// Shuffle the deck of cards in-place, and reset its `next` index to 0
+    pub fn shuffle(&mut self) {
+        self.cards.shuffle(&mut thread_rng());
+        self.next = 0;
+    }
+
+    /// Generate a new shuffled multi-deck with `l * DECK_LEN` cards
+    pub fn with_length(l: usize) -> Self {
+        assert!(l >= 1);
+        // generate 1 Vec<Card>
+        let single = {
+            let mut v = Vec::with_capacity(DECK_LEN);
+            for suit in ALL_SUITS.iter() {
+                for rank in ALL_RANKS.iter() {
+                    v.push(Card::new(*rank, *suit));
+                }
+            }
+            v
+        };
+        // append copies of the single deck to the output multi-deck
+        let mut multi = Vec::with_capacity(l * DECK_LEN);
+        for _ in 0..l {
+            multi.append(&mut single.clone());
+        }
+        assert_eq!(multi.len(), multi.capacity());
+        let mut d = Self {
+            cards: multi,
+            next: 0,
+        };
+        // shuffle it
+        d.shuffle();
+        d
+    }
+
+    pub fn draw(&mut self) -> Result<Card, DeckError> {
+        if self.next == self.cards.len() {
+            Err(DeckError::OutOfCards)
+        } else {
+            let c = self.cards[self.next];
+            self.next += 1;
+            Ok(c)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Card, Deck, DeckError, DECK_LEN};
+    use std::collections::HashMap;
+
+    #[test]
+    fn right_len_1() {
+        let d = Deck::new();
+        assert_eq!(d.cards.len(), d.cards.capacity());
+        assert_eq!(d.cards.len(), DECK_LEN);
+    }
+
+    #[test]
+    fn right_len_2() {
+        let d = Deck::with_length(8);
+        assert_eq!(d.cards.len(), d.cards.capacity());
+        assert_eq!(d.cards.len(), 8 * DECK_LEN);
+    }
+
+    #[test]
+    fn right_count_1() {
+        let d = Deck::new();
+        let mut counts: HashMap<Card, u16> = HashMap::new();
+        for card in d.cards.iter() {
+            if let Some(count) = counts.get_mut(card) {
+                *count += 1;
+            } else {
+                counts.insert(*card, 1);
+            }
+        }
+        assert_eq!(counts.len(), DECK_LEN);
+        for count in counts.values() {
+            assert_eq!(*count, 1);
+        }
+    }
+
+    #[test]
+    fn right_count_2() {
+        let d = Deck::with_length(8);
+        let mut counts: HashMap<Card, u16> = HashMap::new();
+        for card in d.cards.iter() {
+            if let Some(count) = counts.get_mut(card) {
+                *count += 1;
+            } else {
+                counts.insert(*card, 1);
+            }
+        }
+        assert_eq!(counts.len(), DECK_LEN);
+        for count in counts.values() {
+            assert_eq!(*count, 8);
+        }
+    }
+
+    #[test]
+    fn draw_1() {
+        let mut d = Deck::new();
+        for _ in 0..DECK_LEN {
+            assert!(d.draw().is_ok());
+        }
+        assert_eq!(d.draw().unwrap_err(), DeckError::OutOfCards);
+    }
+
+    #[test]
+    fn draw_2() {
+        let mut d = Deck::with_length(8);
+        for _ in 0..8 * DECK_LEN {
+            assert!(d.draw().is_ok());
+        }
+        assert_eq!(d.draw().unwrap_err(), DeckError::OutOfCards);
+    }
+}
---
+++ src/hand.rs
@@ -0,0 +1,159 @@
+use crate::deck::{Card, Rank};
+use std::fmt;
+
+#[derive(Clone)]
+pub struct Hand {
+    cards: Vec<Card>,
+}
+
+impl fmt::Display for Hand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "[{}]",
+            self.cards
+                .iter()
+                .map(|c| c.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        )
+    }
+}
+
+impl Hand {
+    pub fn new(cards: &[Card; 2]) -> Self {
+        Self {
+            cards: cards.to_vec(),
+        }
+    }
+
+    pub fn is_soft(&self) -> bool {
+        self.cards[0].rank == Rank::RA || self.cards[1].rank == Rank::RA
+    }
+
+    pub fn value(&self) -> u8 {
+        let mut num_ace = 0;
+        let mut acc = 0;
+        for c in self.cards.iter() {
+            acc += match c.rank {
+                Rank::R2
+                | Rank::R3
+                | Rank::R4
+                | Rank::R5
+                | Rank::R6
+                | Rank::R7
+                | Rank::R8
+                | Rank::R9
+                | Rank::RT
+                | Rank::RJ
+                | Rank::RQ
+                | Rank::RK => c.value(),
+                Rank::RA => {
+                    num_ace += 1;
+                    c.rank.value()
+                }
+            }
+        }
+        while acc > 21 && num_ace > 0 {
+            num_ace -= 1;
+            acc -= 10;
+        }
+        acc
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Hand;
+    use crate::deck::{Card, Rank, Suit, ALL_RANKS};
+    const SUIT: Suit = Suit::Club;
+
+    fn all_pairs() -> Vec<Hand> {
+        let mut hands = vec![];
+        for r1 in ALL_RANKS.iter() {
+            let c1 = Card::new(*r1, SUIT);
+            for r2 in ALL_RANKS.iter() {
+                let c2 = Card::new(*r2, SUIT);
+                hands.push(Hand::new(&[c1, c2]));
+            }
+        }
+        hands
+    }
+
+    #[test]
+    fn value_le_21() {
+        for hand in all_pairs() {
+            //eprintln!("{}", hand);
+            let mut v = hand.cards[0].value();
+            if hand.cards[1].rank == Rank::RA && v >= 11 {
+                v += 1;
+            } else {
+                v += hand.cards[1].value();
+            }
+            // sanity check for the test itself, not really exercising the actual code
+            assert!(v <= 21);
+            // actual test
+            assert_eq!(v, hand.value());
+        }
+    }
+
+    #[test]
+    fn is_soft() {
+        for hand in all_pairs() {
+            assert_eq!(
+                hand.is_soft(),
+                hand.cards.contains(&Card::new(Rank::RA, SUIT))
+            );
+        }
+    }
+
+    #[test]
+    fn aces_1() {
+        // 2..=11 aces are 12..=21 value
+        // 12.. aces are 12.. value
+        let base = Hand::new(&[Card::new(Rank::RA, SUIT), Card::new(Rank::RA, SUIT)]);
+        // "extra" aces in addition to 2 already in the hand
+        for extra in 0..=21 {
+            let mut hand = base.clone();
+            hand.cards.append(
+                &mut std::iter::repeat(Card::new(Rank::RA, SUIT))
+                    .take(extra)
+                    .collect(),
+            );
+            // sanity check for test
+            assert_eq!(hand.cards.len(), 2 + extra);
+            // actual test
+            if hand.cards.len() <= 11 {
+                assert_eq!(hand.value(), 12 + extra as u8);
+            } else {
+                assert_eq!(hand.value(), 2 + extra as u8);
+            }
+            // might as well
+            assert!(hand.is_soft());
+        }
+    }
+
+    #[test]
+    fn aces_2() {
+        // a single ace in hand of 3 hards is either 11 or 1, depending on value of other 2 cards
+        for mut hand in all_pairs() {
+            let orig_val = hand.value();
+            // for this test, skip hands that start out with more than 0 aces
+            if hand.cards[0].rank == Rank::RA || hand.cards[1].rank == Rank::RA {
+                continue;
+            }
+            hand.cards.push(Card::new(Rank::RA, SUIT));
+            match orig_val {
+                4..=10 => {
+                    eprintln!("{}", hand);
+                    assert_eq!(hand.value(), orig_val + 11);
+                }
+                11..=21 => {
+                    eprintln!("{}", hand);
+                    assert_eq!(hand.value(), orig_val + 1);
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+}
