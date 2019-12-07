0a9bb54e4c5ca4809d19437a0c8ef68aacba2e3b
Matt Traudt <sirmatt@ksu.edu>
2019-12-07 09:02:00 -0500

    Improve PlayStats: weight of 0 is now impossible
    Primary motivation is to avoid weight=0 when player gets hand correct
    on their first try. Weight of zero means they'll never see it again.
    All weights of zero is an error in WeightedDistribution.
    
    The change also means that getting 1 of 2 hands correct is no longer
    the same weight as getting 10 of 20 hands correct.

--- src/playstats.rs
+++ src/playstats.rs
@@ -14,10 +14,22 @@ impl PlayStats {
     }
 
     pub fn weight(self) -> f32 {
-        if self.seen == 0 {
-            return 1f32;
-        }
-        1f32 - self.correct as f32 / self.seen as f32
+        // Written such that this always produces a weight in (0, 1) if the PlayStats has been
+        // incremented, or 1 if it hasn't.
+        // - weight of untouched PlayStats is 1 = (1 - 0/1)
+        // - weight of PlayStats with 1 wrong is 1.0 = (1 - 0/2)
+        // - weight of PlayStats with 1 correct is 0.5 = (1 - 1/2)
+        // - weight of PlayStats with 2 correct is 0.333 = (1 - 2/3)
+        // - weight of PlayStats with 1 correct/1 wrong is 0.666 = (1 - 1/3)
+        //
+        // The +1 prevents division by zero. It also means that the inclusive side of the (0, 1)
+        // range is at 1 instead of 0. Without the +1, getting a hand correct on the very first try
+        // would result in 1 - 1/1 == 0. Zero weight means the player will never see it again. We
+        // could add a very small amount to zero values to keep them non-zero, but I think adding 1
+        // will create better weights that result in a better player experience.
+        //
+        // weight() will NOT equal correct() / seen()
+        1f32 - self.correct as f32 / (self.seen + 1) as f32
     }
 
     pub fn inc(&mut self, correct: bool) {
@@ -53,37 +65,18 @@ mod tests {
 
     #[test]
     fn weight_half() {
+        // getting half correct doesn't result in 0.5. It gets closer to 0.5 the more games you've
+        // played (and still only gotten half correct)
         let mut s = PlayStats::new();
         s.inc_by(10, false);
         s.inc_by(10, true);
-        assert_eq!(s.weight(), 0.5);
+        // 1 - 10/21 == 0.52
+        assert_eq!(s.weight(), 1.0 - 10.0 / 21.0);
         let mut s = PlayStats::new();
         s.inc(false);
         s.inc(true);
-        assert_eq!(s.weight(), 0.5);
-    }
-
-    #[test]
-    fn weight_quarters() {
-        let mut s = PlayStats::new();
-        s.inc_by(3, false);
-        s.inc_by(1, true);
-        assert_eq!(s.weight(), 0.75);
-        let mut s = PlayStats::new();
-        s.inc_by(30, true);
-        s.inc_by(10, false);
-        assert_eq!(s.weight(), 0.25);
-    }
-
-    #[test]
-    fn weight_same() {
-        let mut s1 = PlayStats::new();
-        let mut s2 = PlayStats::new();
-        s1.inc_by(10, false);
-        s1.inc_by(30, true);
-        s2.inc_by(1, false);
-        s2.inc_by(3, true);
-        assert_eq!(s1.weight(), s2.weight());
+        // 1 - 1 /3 == 0.67
+        assert_eq!(s.weight(), 1.0 - 1.0 / 3.0);
     }
 
     #[test]
@@ -98,11 +91,12 @@ mod tests {
 
     #[test]
     fn weight_correct() {
-        // if always correct when inc, weight is 0
+        // if always correct when inc, weight is near 0
         let mut s = PlayStats::new();
-        for _ in 0..COUNT_MANY {
+        for (i, _) in (0..COUNT_MANY).enumerate() {
+            let i = i + 1;
             s.inc(true);
-            assert_eq!(s.weight(), 0f32);
+            assert_eq!(s.weight(), 1.0 - i as f32 / (i as f32 + 1.0));
         }
     }
 
