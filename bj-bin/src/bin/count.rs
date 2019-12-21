38ae5f63d9f7c8adc4e6101a714c641ff905cabc
Matt Traudt <sirmatt@ksu.edu>
2019-12-21 09:57:03 -0500

    Can specify number of cards to count at a time

--- bj-bin/src/bin/count.rs
+++ bj-bin/src/bin/count.rs
@@ -1,13 +1,14 @@
 use bj_bin::prompt;
 use bj_core::deck::{Card, Deck};
 use bj_core::hand::Hand;
+use clap::{crate_authors, crate_name, crate_version, value_t, App, Arg};
 use std::io::{self, BufRead, BufReader, Write};
 
 fn count_of_card(c: Card) -> i8 {
     match c.value() {
-        2..=5 => 1,
-        6..=8 => 0,
-        9..=10 | 1 => -1,
+        2..=6 => 1,
+        7..=9 => 0,
+        10 | 1 => -1,
         _ => unreachable!(),
     }
 }
@@ -16,12 +17,15 @@ fn count_of_hand(h: &Hand) -> i8 {
     h.cards().fold(0, |acc, &c| acc + count_of_card(c))
 }
 
-fn prompt_for_num(
-    h: &Hand,
+fn prompt_for_num<D>(
+    hand_or_card: &D,
     in_buf: &mut impl BufRead,
     out_buf: &mut impl Write,
-) -> io::Result<prompt::Command> {
-    let s = &format!("count of {}", h);
+) -> io::Result<prompt::Command>
+where
+    D: std::fmt::Display,
+{
+    let s = &format!("count of {}", hand_or_card);
     loop {
         match prompt::prompt(s, in_buf, out_buf)? {
             prompt::Command::SaveQuit | prompt::Command::Quit => break Ok(prompt::Command::Quit),
@@ -35,22 +39,48 @@ fn prompt_for_num(
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = App::new(String::from(crate_name!()) + " count")
+        .author(crate_authors!())
+        .version(crate_version!())
+        .arg(
+            Arg::with_name("cards")
+                .short("c")
+                .long("cards")
+                .value_name("NUM")
+                .help("Number of cards to show at once")
+                .default_value("1"),
+        )
+        .get_matches();
+    let num_cards = value_t!(matches, "cards", usize)?;
+    if num_cards == 0 {
+        return Err("Must specify at least 1 card".into());
+    }
     let mut input = BufReader::new(io::stdin());
     let mut output = io::stdout();
     let mut deck = Deck::new_infinite();
     loop {
-        let hand = Hand::new(&[deck.draw()?, deck.draw()?]);
-        let cmd = prompt_for_num(&hand, &mut input, &mut output)?;
+        let (actual, cmd) = if num_cards == 1 {
+            let c = deck.draw()?;
+            let cmd = prompt_for_num(&c, &mut input, &mut output)?;
+            (count_of_card(c), cmd)
+        } else {
+            let h = Hand::new(
+                &(0..num_cards)
+                    .map(|_| deck.draw().unwrap())
+                    .collect::<Vec<_>>(),
+            );
+            let cmd = prompt_for_num(&h, &mut input, &mut output)?;
+            (count_of_hand(&h), cmd)
+        };
         let val = match cmd {
             prompt::Command::Quit => return Ok(()),
             prompt::Command::Num(n) => n as i8,
             _ => unreachable!(),
         };
-        let actual = count_of_hand(&hand);
         if val == actual {
             println!("Correct");
         } else {
-            println!("count of {} is {}", hand, actual);
+            println!("count is {}", actual);
         }
     }
 }
