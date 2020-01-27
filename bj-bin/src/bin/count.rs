1fa2eb0c5abdecded20db1a37a075db85c81a8de
Matt Traudt <sirmatt@ksu.edu>
2020-01-26 20:37:04 -0500

    Edit count bin to use new count library code

--- bj-bin/src/bin/count.rs
+++ bj-bin/src/bin/count.rs
@@ -1,21 +1,9 @@
 use bj_bin::prompt;
+use bj_core::count::{CountSystem, HiLo};
 use bj_core::deck::{Card, Deck};
 use bj_core::hand::Hand;
 use clap::{crate_authors, crate_name, crate_version, value_t, App, Arg};
 use std::io::{self, BufRead, BufReader, Write};
-
-fn count_of_card(c: Card) -> i8 {
-    match c.value() {
-        2..=6 => 1,
-        7..=9 => 0,
-        10 | 1 => -1,
-        _ => unreachable!(),
-    }
-}
-
-fn count_of_hand(h: &Hand) -> i8 {
-    h.cards().fold(0, |acc, &c| acc + count_of_card(c))
-}
 
 fn prompt_for_num<D>(
     hand_or_card: &D,
@@ -58,60 +46,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = BufReader::new(io::stdin());
     let mut output = io::stdout();
     let mut deck = Deck::new_infinite();
+    let hilo = HiLo::new();
     loop {
         let (actual, cmd) = if num_cards == 1 {
             let c = deck.draw()?;
             let cmd = prompt_for_num(&c, &mut input, &mut output)?;
-            (count_of_card(c), cmd)
+            (hilo.card_value(c), cmd)
         } else {
-            let h = Hand::new(
-                &(0..num_cards)
-                    .map(|_| deck.draw().unwrap())
-                    .collect::<Vec<_>>(),
-            );
+            let cards = (0..num_cards)
+                .map(|_| deck.draw().unwrap())
+                .collect::<Vec<Card>>();
+            let h = Hand::new(&cards);
             let cmd = prompt_for_num(&h, &mut input, &mut output)?;
-            (count_of_hand(&h), cmd)
+            (hilo.cards_value(&cards), cmd)
         };
         let val = match cmd {
             prompt::Command::Quit => return Ok(()),
-            prompt::Command::Num(n) => n as i8,
+            prompt::Command::Num(n) => n as i16,
             _ => unreachable!(),
         };
         if val == actual {
             println!("Correct");
         } else {
             println!("count is {}", actual);
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{count_of_card, count_of_hand};
-    use bj_core::deck::Deck;
-    use bj_core::hand::Hand;
-
-    #[test]
-    fn card_always_has_count() {
-        // all cards have some count
-        let mut d = Deck::new();
-        while let Ok(c) = d.draw() {
-            let count = count_of_card(c);
-            assert!(count >= -1 && count <= 1);
-        }
-    }
-
-    #[test]
-    fn card_and_hand_count_match() {
-        // getting the count for a hand matches the sum of the count value for each individual card
-        let mut deck = Deck::new_infinite();
-        for len in 2..=4 {
-            for _rep in 0..100 {
-                let hand = Hand::new(&vec![deck.draw().unwrap(); len]);
-                let expect = hand.cards().fold(0, |acc, &c| acc + count_of_card(c));
-                let actual = count_of_hand(&hand);
-                assert_eq!(expect, actual);
-            }
         }
     }
 }
