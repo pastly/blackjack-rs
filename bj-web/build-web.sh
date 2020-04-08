46474c2180314e000332cda953ed646754e082ce
Matt Traudt <sirmatt@ksu.edu>
2020-04-07 21:45:40 -0400

    Make www/ dir a template dir; script to generate final HTML ...
    Right now way over kill (oops...) and just injects the version into
    the bottom of the single HTML page. Jeeeeeez. Waste of time.

--- bj-web/build-web.sh
+++ bj-web/build-web.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
 set -eu
-function inject_version() {
-    # BEGIN EDIT
-    SRC=www/index.html
-    DST=www/index.html
-    # END EDIT
-    TMP=$(mktemp)
-    COMMIT=$(git rev-parse --short HEAD)
-    DATE=$(git show -s --format='%cd' --date='format:%Y-%m-%d' $COMMIT)
-    VERSION_LINE="<!-- VERSION_LINE -->Version $DATE ($COMMIT)"
-    sed "s|^.*VERSION_LINE.*$|$VERSION_LINE|g" $SRC > $TMP
-    cp -v $TMP $DST
-    rm $TMP
-}
 
 cargo fmt
 cargo clippy
 cargo check
 wasm-pack -v build --dev --target web
-inject_version
+./format-web-templates.py
---
+++ bj-web/format-web-templates.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from glob import iglob
+import os
+import logging
+from subprocess import run
+
+
+BIN_EXTS = [
+    '.wasm',
+    '.png',
+]
+VERSION_STR = None
+
+logging.basicConfig(filename='/dev/stdout', level=logging.INFO)
+
+
+def abspath(p):
+    op = os.path
+    return op.abspath(op.expanduser(op.expandvars(p)))
+
+
+def find_files(dname):
+    for name in iglob(f'{dname}/**', recursive=True):
+        if os.path.isfile(name):
+            yield name
+
+
+def replace_prefix(s, bad_prefix, new_prefix):
+    assert s.startswith(bad_prefix)
+    return new_prefix + s[len(bad_prefix):]
+
+
+def is_binary_file(fname):
+    ext = os.path.splitext(fname)[1]
+    return ext in BIN_EXTS
+
+
+def get_version_str():
+    global VERSION_STR
+    if VERSION_STR is not None:
+        return VERSION_STR
+    proc = run(
+        'git rev-parse --short HEAD'.split(),
+        text=True, capture_output=True)
+    commit = proc.stdout.strip()
+    proc = run(
+        'git show -s --format=%cd --date=format:%Y-%m-%d'.split() + [commit],
+        text=True, capture_output=True)
+    date = proc.stdout.strip()
+    VERSION_STR = f'{date} ({commit})'
+    return VERSION_STR
+
+
+def main(args):
+    for in_fname in find_files(args.input):
+        out_fname = replace_prefix(in_fname, args.input, args.output)
+        dirname = os.path.dirname(out_fname)
+        logging.debug(f'Making sure {dirname} exists')
+        os.makedirs(dirname, exist_ok=True)
+        rmode = 'rb' if is_binary_file(in_fname) else 'rt'
+        wmode = 'wb' if is_binary_file(out_fname) else 'wt'
+        with open(in_fname, rmode) as ifd, open(out_fname, wmode) as ofd:
+            if is_binary_file(in_fname):
+                logging.info(f'Considering {in_fname} a binary file')
+                ofd.write(ifd.read())
+                continue
+            logging.info(f'Considering {in_fname} a text file')
+            s = ifd.read()
+            s = s.replace('<!-- BJ_TMPL_VERSION -->', get_version_str())
+            ofd.write(s)
+
+
+if __name__ == '__main__':
+    p = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+    p.add_argument(
+        '-i', '--input', type=str, default='www', help='Input directory')
+    p.add_argument(
+        '-o', '--output', type=str, default='www-out', help='Output directory')
+    args = p.parse_args()
+    args.input = abspath(args.input)
+    args.output = abspath(args.output)
+    assert os.path.isdir(args.input), f'{args.input} does not exist'
+    if os.path.isdir(args.output):
+        logging.warning(
+            f'{args.output} exists. Files inside will be overwritten')
+    exit(main(args))
