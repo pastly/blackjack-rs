a3d51b42d7272a16288b114f6a159af8b6d08b7e
Matt Traudt <sirmatt@ksu.edu>
2020-04-07 21:50:32 -0400

    Allow build-web.sh to be either debug or release

--- bj-web/build-web.sh
+++ bj-web/build-web.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
+if [[ "$#" != "1" ]] || [[ "$1" != "--release" ]]; then
+	MODE="--dev"
+else
+	MODE="--release"
+fi
+echo "Compiling with $MODE"
 cargo fmt
 cargo clippy
 cargo check
-wasm-pack -v build --dev --target web
+wasm-pack -v build $MODE --target web
 ./format-web-templates.py
