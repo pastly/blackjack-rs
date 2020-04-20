33ee8d6a8e10b056a452881e80d581c111612e70
Matt Traudt <sirmatt@ksu.edu>
2020-04-19 20:34:18 -0400

    Update deploy script

--- bj-web/deploy.sh
+++ bj-web/deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 ./build-web.sh --release
-for A in free-play/pkg/bj_web_free_play_bg.wasm \
-        free-play/pkg/bj_web_free_play.js; do
+for A in basic-strategy/pkg/*.{js,wasm}; do
     aws s3 cp $A s3://blackjack-wasm/pub/$(basename $A)
 done
