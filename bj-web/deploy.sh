8b581f06d83d9aa56e1f7f09d1ab3bbf22655868
Matt Traudt <sirmatt@ksu.edu>
2020-04-28 16:26:53 -0400

    Add time-trial to deploy script

--- bj-web/deploy.sh
+++ bj-web/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 ./build-web.sh --release
-for A in basic-strategy/pkg/*.{js,wasm}; do
+for A in basic-strategy/pkg/*.{js,wasm} time-trial/pkg/*.{js,wasm}
+do
     aws s3 cp $A s3://blackjack-wasm/pub/$(basename $A)
 done
