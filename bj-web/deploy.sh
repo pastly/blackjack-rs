3ce58845d1cb1d02e45157dc7f19f9c6dce6d9cf
Matt Traudt <sirmatt@ksu.edu>
2020-08-09 13:48:10 -0400

    Include counting crate artifacts in deploy script

--- bj-web/deploy.sh
+++ bj-web/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 ./build-web.sh --release
-for A in basic-strategy/pkg/*.{js,wasm} time-trial/pkg/*.{js,wasm}
+for A in basic-strategy/pkg/*.{js,wasm} time-trial/pkg/*.{js,wasm} counting/pkg/*.{js,wasm}
 do
     aws s3 cp $A s3://blackjack-wasm/pub/$(basename $A)
 done
