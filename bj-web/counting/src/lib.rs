90a5820a6844465d0572fa7bf1c92747c309ab40
Matt Traudt <sirmatt@ksu.edu>
2020-08-10 20:30:05 -0400

    Support for timing card counting session

--- bj-web/counting/src/lib.rs
+++ bj-web/counting/src/lib.rs
@@ -1,6 +1,7 @@
 use bj_core::count::{StatefulHiLo, DECK_LEN};
 use bj_core::deck::{Card, Deck};
 use bj_web_core::card_char;
+use js_sys::Date;
 use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -17,16 +18,30 @@ struct State {
     seen_cards: u16,
     /// num cards to show at the user at once
     cards_at_a_time: u8,
+    /// timestamp (in s, not ms) of when we present the first card
+    start_time: f64,
+    /// timestamp (in s, not ms) of when we first get asked for another card, but there are no more
+    /// cards
+    end_time: f64,
 }
 
 impl Default for State {
     fn default() -> Self {
         Self {
+            // reset in rust_init()
             deck: Deck::with_length(1),
+            // reset in rust_init()
             count: StatefulHiLo::new(1),
+            // reset in rust_init()
             total_cards: DECK_LEN,
+            // reset in rust_init()
             seen_cards: 0,
+            // reset in rust_init()
             cards_at_a_time: 1,
+            // updated when we show first card
+            start_time: 0.0,
+            // updated when we cannot show any more cards
+            end_time: 0.0,
         }
     }
 }
@@ -69,6 +84,8 @@ pub fn rust_init(num_decks: u8, num_cards: u16, cards_at_a_time: u8) -> bool {
     state.total_cards = num_cards;
     state.seen_cards = 0;
     state.cards_at_a_time = cards_at_a_time;
+    state.start_time = 0.0;
+    state.end_time = 0.0;
     log(&format!(
         "Init rust state with {} decks and showing {} cards {} at a time",
         num_decks, num_cards, cards_at_a_time
@@ -90,8 +107,15 @@ fn output_cards(cards: &[Card]) {
 #[wasm_bindgen]
 pub fn display_next_card() -> bool {
     let mut state = STATE.lock().unwrap();
+    let now = Date::now() / 1000.0; // convert from ms to s
+    if state.seen_cards == 0 {
+        state.start_time = now;
+    }
     if state.seen_cards >= state.total_cards {
         log("No next card");
+        if state.end_time == 0.0 {
+            state.end_time = now;
+        }
         return false;
     }
     let cards = {
@@ -117,4 +141,16 @@ pub fn display_next_card() -> bool {
 #[wasm_bindgen]
 pub fn current_count() -> i16 {
     STATE.lock().unwrap().count.running_count()
+}
+
+/// Returns how long it took the player to have us display all cards, or something less than 0 if
+/// not all cards have been shown yet
+#[wasm_bindgen]
+pub fn game_duration() -> f64 {
+    // If the game isn't over yet, then end_time will be 0.0 and this will end up negative, which
+    // handles the error case by itself
+    let state = STATE.lock().unwrap();
+    let dur = state.end_time - state.start_time;
+    log(&format!("Duration was: {}", dur));
+    dur
 }
